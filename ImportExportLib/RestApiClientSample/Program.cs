e1ab49b793cdd5a3f4a80ff191b5b0f9cd4156ce
Rena Shah <rena.shah@microsoft.com>
2016-08-08 20:53:29 -0700

    Uploading the sample

---
+++ ImportExportLib/RestApiClientSample/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using StorageImportExport.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StorageImportExport
+{
+    class Program
+    {
+
+    static void Main(string[] args)
+        {
+            try
+            {
+                // load client certificate from local store
+                X509Certificate2 clientCert;
+                var clientCertificateThumbprint = "<client certificate thumbprint>";
+                if (!GetCertificateFromStore(clientCertificateThumbprint, StoreName.My, StoreLocation.CurrentUser, out clientCert)
+                    && !GetCertificateFromStore(clientCertificateThumbprint, StoreName.My, StoreLocation.LocalMachine, out clientCert))
+                {
+                    var message = string.Format("Could not find client certificate with thumbprint '{0}' in any cert store.", clientCertificateThumbprint);
+                    throw new Exception(message);
+                }
+
+                // create an import/export client with the azure subscription id and the client certificate
+                var client = new StorageImportExportClient("<azure subscription id>", clientCert);
+
+                // create a sample export job
+                client.CreateJob("myjobname20160803export", "config\\TemplateExportConfig.xml");
+
+                // create a sample import job
+                client.CreateJob("myjobname20160803import", "config\\TemplateImportConfig.xml");
+
+                // list all jobs in the storage account
+                var jobList = client.ListJobs("storageaccountname");
+                jobList = client.ListJobs("storageaccountname", JobType.Import);
+
+                // ship the sample import job
+                var package = new PackageInfomation("FedEx", "0123456789", 1, "2016/08/01");
+                client.ShipJob("storageaccountname", "myjobname20160803import", package);
+
+                // get the sample import job and read its job state
+                var jobItem = client.GetJob("storageaccountname", "myjobname20160803import");
+                Console.WriteLine("{0}: {1}", jobItem.Name, jobItem.Properties.State);
+            }
+            catch (ErrorResponseException e)
+            {
+                Console.WriteLine(e.Body.Value);
+                Console.WriteLine(e.Body.ExtendedInformation);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Get a certificate from local certificate store
+        /// </summary>
+        /// <param name="thumbprint">Thumbprint of the certificate to look for</param>
+        /// <param name="storeName">Store Name to look in to</param>
+        /// <param name="storeLocation">Store Location to look in to</param>
+        /// <param name="certificate">Certificate</param>
+        /// <returns>True if certificate is found in the cert store</returns>
+        private static bool GetCertificateFromStore(
+            string thumbprint,
+            StoreName storeName,
+            StoreLocation storeLocation,
+            out X509Certificate2 certificate)
+        {
+            certificate = null;
+            // Create a reference to the My certificate store.
+            X509Store certStore = new X509Store(storeName, storeLocation);
+            certStore.Open(OpenFlags.ReadOnly);
+
+            // Find the certificate that matches the thumbprint.
+            X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                X509FindType.FindByThumbprint,
+                thumbprint,
+                false);
+
+            certStore.Close();
+
+            // Check to see if our certificate was added to the collection. If no, throw an error, if yes, create a certificate using it.
+            if (0 == certCollection.Count)
+            {
+                Console.WriteLine(String.Format("No certificate containing thumbprint '{0}' found in '{1}\\{2}'", thumbprint,
+                        storeLocation, storeName));
+                return false;
+            }
+            // Create an X509Certificate2 object using our matching certificate.
+            certificate = certCollection[0];
+            return true;
+        }
+    }
+}
---
+++ ImportExportLib/RestApiClientSample/StorageImportExportClient.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using StorageImportExport.Models;
+
+namespace StorageImportExport
+{
+    class StorageImportExportClient
+    {
+        private StorageImportExportLib client;
+        private WebRequestHandler webHandler;
+
+        /// <summary>
+        /// Initialize a import/export service client
+        /// </summary>
+        /// <param name="subscriptionId">The subscription ID for the Azure user.</param>
+        /// <param name="clientCertificate">The client certificate for the Azure user.</param>
+        public StorageImportExportClient(string subscriptionId, X509Certificate2 clientCertificate)
+        {
+            webHandler = new WebRequestHandler();
+            client = new StorageImportExportLib(webHandler);
+            client.SubscriptionId = subscriptionId;
+            webHandler.ClientCertificates.Add(clientCertificate);
+        }
+
+        /// <summary>
+        /// Create a new job in the Windows Azure Import/Export service.
+        /// </summary>
+        /// <param name="jobName">Name of the job.</param>
+        /// <param name="configFilePath"></param>
+        public void CreateJob(string jobName, string configFilePath)
+        {
+            var XConf = XDocument.Load(configFilePath);
+
+            // ReturnAddress: Specifies the return address information for the job.
+            var XReturnAddress = XConf.Descendants("ReturnAddress").First();
+            var returnAddress = new ReturnAddress(
+                XReturnAddress.Element("Name").Value,
+                XReturnAddress.Element("Address").Value,
+                XReturnAddress.Element("Phone").Value,
+                XReturnAddress.Element("Email").Value
+            );
+
+            // ReturnShipping: Specifies the return carrier and customer’s account with the carrier
+            var XReturnShipping = XConf.Descendants("ReturnShipping").First();
+            var returnShipping = new ReturnShipping(
+                XReturnShipping.Element("CarrierName").Value,
+                XReturnShipping.Element("CarrierAccountNumber").Value
+            );            
+
+            // Properties: The list of properties for the job.
+            // refer to https://msdn.microsoft.com/en-us/library/azure/dn529110.aspx for more details
+            var XJobProperty = XConf.Descendants("JobProperty").First();
+            var putJobProperties = new PutJobProperties(
+                backupDriveManifest: bool.Parse(XJobProperty.Element("BackupDriveManifest").Value),
+                description: XJobProperty.Element("Description").Value,
+                enableVerboseLog: bool.Parse(XJobProperty.Element("EnableVerboseLog").Value),
+                friendlyName: XJobProperty.Element("FriendlyName").Value,
+                type: (XJobProperty.Element("JobType").Value.Equals("Import", StringComparison.InvariantCultureIgnoreCase)? JobType.Import: JobType.Export),
+                location: XJobProperty.Element("Location").Value,
+                storageAccountKey: XJobProperty.Element("StorageAccountKey").Value,
+                storageAccountName: XJobProperty.Element("StorageAccountName").Value,
+                importExportStatesPath: XJobProperty.Element("ImportExportStatesPath").Value,               
+                returnAddress: returnAddress,
+                returnShipping: returnShipping
+            );
+
+            // must include either StorageAccountKey or ContainerSas in the request
+            if (string.IsNullOrEmpty(XJobProperty.Element("StorageAccountKey").Value))
+            {
+                putJobProperties.StorageAccountKey = null;
+                putJobProperties.ContainerSas = XJobProperty.Element("ContainerSas").Value;
+            }
+
+            var putJobParameters = new PutJobParameters(jobName, putJobProperties);
+            if (putJobProperties.Type == JobType.Export)
+            {
+                // BlobList: contain information about the blobs to be exported for an export job.  
+                var XBlobList = XConf.Descendants("BlobList").First().Elements();
+                var blobList = new BlobList(new List<string>(), new List<string>());
+                foreach (var XBlob in XBlobList)
+                {
+                    if (!String.IsNullOrWhiteSpace(XBlob.Attribute("BlobPaths").Value))
+                    {
+                        blobList.BlobPath.Add(XBlob.Attribute("BlobPaths").Value);
+                    }
+                    if (!String.IsNullOrWhiteSpace(XBlob.Attribute("BlobPathPrefixes").Value))
+                    {
+                        blobList.BlobPathPrefix.Add(XBlob.Attribute("BlobPathPrefixes").Value);
+                    }
+                }
+                putJobParameters.Export = new Export(blobList: blobList);
+            }
+            else
+            {
+                // DriveList: List of up to ten drives that comprise the job.
+                var XDriveList = XConf.Descendants("DriveList").First().Elements();
+                var driveList = new List<Drive>();
+                foreach (var XDrive in XDriveList)
+                {
+                    driveList.Add(new Drive(
+                        driveId: XDrive.Element("DriveId").Value,
+                        bitLockerKey: XDrive.Element("BitLockerKey").Value,
+                        manifestFile: XDrive.Element("ManifestFile").Value,
+                        manifestHash: XDrive.Element("ManifestHash").Value
+                    ));
+                }
+                putJobParameters.DriveList = driveList;
+            }
+
+            client.PutJob(XJobProperty.Element("StorageAccountName").Value, jobName, putJobParameters);
+        }
+
+        /// <summary>
+        /// Notify the Import/Export service that the hard drives comprising the import or export job have been shipped to the Microsoft data center.
+        /// </summary>
+        /// <param name="storageAccountName">Name of the storage account you want to operate on.</param>
+        /// <param name="jobName">Name of the job.</param>
+        /// <param name="deliveryPackage">Contains information about the package being shipped by the customer to the Microsoft data center.</param>
+        public void ShipJob(string storageAccountName, string jobName, PackageInfomation deliveryPackage)
+        {
+            var patchJobParameters = new PatchJobParameters(deliveryPackage: deliveryPackage, state: "Shipping");
+            client.PatchJob(storageAccountName, jobName, patchJobParameters);
+        }
+
+        /// <summary>
+        /// Get information about an existing job from the Windows Azure Import/Export service. 
+        /// </summary>
+        /// <param name="storageAccountName">Name of the storage account you want to operate on.</param>
+        /// <param name="jobName">Name of the job.</param>
+        /// <returns></returns>
+        public GetJobResponse GetJob(string storageAccountName, string jobName)
+        {
+            return client.GetJob(storageAccountName, jobName);
+        }
+
+        /// <summary>
+        /// List active and completed jobs for a storage account in a subscription.
+        /// </summary>
+        /// <param name="storageAccountName">Name of the storage account you want to operate on.</param>
+        /// <param name="jobType">Optional. List jobs with the job type.</param>
+        /// <returns></returns>
+        public IList<ListJobsResponseValueItem> ListJobs(string storageAccountName, JobType? jobType = null)
+        {
+            var jobList = client.ListJobs(storageAccountName, jobType);
+            return jobList.Value;
+        }
+
+
+    }
+}
